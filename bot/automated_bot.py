37148193035a2eded708f2002260c3dff37fa93a
Arinc Elhan <arinc.elhan@nurd.com>
2019-01-18 16:00:34 +0300

    Add user and post create function for bot

--- bot/automated_bot.py
+++ bot/automated_bot.py
@@ -34,16 +34,45 @@ def generate_user():
     }
 
 
-for num in range(USER_NUM):
-    user = generate_user()
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.post(API_URL + '/signup', data=json.dumps(user), headers=headers)
-    if r.status_code == 200:
-        token = r.json()['token']
-        user['token'] = token
-        USERS[user['email']] = user
-    time.sleep(1)
+def generate_post(length_of_post=20):
+    content = string_generator(length_of_post)
+    return {
+        'content': content
+    }
 
-for key, value in USERS.items():
-    print(key)
-    print(value)
+
+def create_user_and_post():
+    for num in range(USER_NUM):
+        user = generate_user()
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        try:
+            user_create_response = requests.post(API_URL + '/signup', data=json.dumps(user), headers=headers)
+            if user_create_response.status_code == 200:
+                print('User is created: Name: %s Email: %s' % (user['name'], user['email']))
+                token = user_create_response.json()['token']
+                user['token'] = token
+                USERS[user['email']] = user
+                random_post_num = random.randint(1, POST_COUNT)
+                print('Create %s number of posts' % (str(random_post_num)))
+                headers['x-access-token'] = token
+                for post_num in range(random_post_num):
+                    post = generate_post()
+                    try:
+                        requests.post(API_URL + '/create', data=json.dumps(post), headers=headers)
+                    except requests.exceptions.RequestException as e:
+                        print('Post Create Error')
+                        print(e)
+                    print('Post is created: Content: %s' % (post['content']))
+                    time.sleep(1)
+                print()
+            else:
+                print('Problem encountered with user creation: Name: %s Email: %s' % (user['name'], user['email']))
+        except requests.exceptions.RequestException as e:
+            print('User Create Error')
+            print(e)
+
+
+if __name__ == '__main__':
+    print('Starting ...\n')
+    create_user_and_post()
+    print('All done!')
